e42bb45bd4cfa9837c725957d257f157143395a2
AdNAlpha <133684229+AdNAlpha@users.noreply.github.com>
2024-11-28 00:19:46 +0800

    Add files via upload

---
+++ 6.6HD/enums/StatusEnums.cs
@@ -0,0 +1,19 @@
+﻿namespace SupplyChainHub.enums
+{
+    public enum OrderStatus
+    {
+        Pending,
+        Shipped,
+        Delivered,
+        Canceled
+    }
+
+    public enum ShipmentStatus
+    {
+        NotShipped,
+        Shipped,
+        InTransit,
+        Delivered,
+        Returned
+    }
+}
